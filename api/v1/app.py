6d5b7cfa1025d01882be0e998c23b62eee5ed365
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-10-31 20:23:03 +0800

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,38 +1,46 @@
 #!/usr/bin/python3
-'''
-    app to register blueprint and start up flask
-'''
-from flask import Flask, make_response, jsonify
-from flask_cors import CORS
+"""Endpoint (route) will be to return the status of your API"""
+import os
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
-import os
+from flask_cors import CORS
 
+
+# creating a Flask app
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
 app.register_blueprint(app_views, url_prefix="/api/v1")
-
-
-@app.teardown_appcontext
-def closeStorageAfterRequest(error):
-    """
-    Teardown method to close the storage
-    """
-    storage.close()
-    storage.reload()
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    """
-    Handles errors and returns a JSON-formatted 404 status code response
-    """
-    return jsonify({"error": "Not found"}), 404
+    return {"error": "Not found"}, 404
 
 
-if __name__ == '__main__':
-    app.run(
-        host=getenv('HBNB_API_HOST', '0.0.0.0'),
-        port=getenv('HBNB_API_PORT', 5000),
-        threaded=True
-    )
+@app.errorhandler(400)
+def page_not_found(e):
+    message = e.description
+    return message, 400
+
+
+@app.teardown_appcontext
+def close(ctx):
+    storage.close()
+
+
+if os.getenv("HBNB_API_HOST"):
+    host = os.getenv("HBNB_API_HOST")
+else:
+    host = "0.0.0.0"
+
+if os.getenv("HBNB_API_PORT"):
+    port = int(os.getenv("HBNB_API_PORT"))
+else:
+    port = 5000
+
+
+if __name__ == "__main__":
+    app.run(host=host, port=port, threaded=True)
