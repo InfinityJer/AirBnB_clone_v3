08db2d9d7fab2e373ec3734473c8caad0bdcf67d
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-10-31 20:21:43 +0800

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,18 +6,20 @@ from flask import Flask, make_response, jsonify
 from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
 @app.teardown_appcontext
-def teardown_storage(exception):
+def closeStorageAfterRequest(error):
     """
     Teardown method to close the storage
     """
     storage.close()
+    storage.reload()
 
 
 @app.errorhandler(404)
@@ -28,7 +30,9 @@ def page_not_found(e):
     return jsonify({"error": "Not found"}), 404
 
 
-if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+if __name__ == '__main__':
+    app.run(
+        host=getenv('HBNB_API_HOST', '0.0.0.0'),
+        port=getenv('HBNB_API_PORT', 5000),
+        threaded=True
+    )
